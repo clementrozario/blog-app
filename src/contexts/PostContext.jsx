16586bdcc5ca80e2e362deb29915fcfb8dabe12c
Clement Delphin <clementcla7@gmail.com>
2026-03-14 15:37:21 +0530

    implement create post

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -7,17 +7,22 @@ function PostProvider(props) {
         {
             id: 1,
             title: 'Hello world',
-            content:'welocme to my first blog'
+            content: 'welocme to my first blog'
         },
         {
             id: 2,
             title: "second post",
-            content:'my second post for the purpose to learning it'
+            content: 'my second post for the purpose to learning it'
         },
     ])
-    
+
+    function addPost(newPost) {
+        const lastId = posts.at(-1).id
+        setPosts((prev) => [...prev, { ...newPost, id: lastId + 1 }]);
+    }
+
     return (
-        <PostContext.Provider value={{posts,setPosts}}>
+        <PostContext.Provider value={{ posts, addPost }}>
             {props.children}
         </PostContext.Provider>
     )
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,11 +1,94 @@
-
-
-
+import { useForm } from 'react-hook-form'
+import { PostContext } from '../contexts/PostContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 function CreatePost() {
+
+    const {addPost} = useContext(PostContext)
+
+    const { 
+        register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm()
+
+    const navigate = useNavigate()
+
+    function onSubmit(data) {
+        addPost(data);
+        navigate("/")
+    }
+
     return (
         <div>
-            <h3>Create Post Page</h3>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+                <h3>Create Post Page</h3>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: 20,
+                    gap: 20
+                }}>
+                    <label>Title</label>
+                    <input
+                        type="text"
+                        placeholder="enter the title ..."
+                        {...register("title", {
+                            required: 'Title is required',
+                            minLength: {
+                                value: 3,
+                                message:'title should be atleast 3 characters'
+                            }
+                        })}
+                    />
+                    {errors.title && (
+                        <p style={{ color: "red" }}>{errors.title.message}</p>
+                    )}
+                </div>
+
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: 20,
+                    gap: 10
+                }}>
+                    <label>Content</label>
+                    <input
+                        type="text"
+                        placeholder="enter the content ..."
+                        {...register("content", {
+                            required: 'Content is required',
+                            minLength: {
+                                value: 10,
+                                message: 'content should be atleast 10 characters'
+                            }
+                        })}
+                    />
+                    {errors.content && (
+                        <p style={{ color: "red" }}>{errors.content.message}</p>
+                    )}
+                </div>
+               
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: 20,
+                    gap: 20
+                }}>
+                    <button>Create Post</button>
+                </div>
+
+            </form>
         </div>
     )
 }
